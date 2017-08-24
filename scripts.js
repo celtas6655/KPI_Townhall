7a61095c5ef9d59c0b4a6ecdc9ea8c0b0a36e260
Michal <hellpartyy@gmail.com>
2017-08-24 15:03:49 +0200

    first beta

---
+++ scripts.js
@@ -0,0 +1,36 @@
+/**
+ * Created by TFD054IT0154 on 24.08.2017.
+ */
+function todayDate() {
+    n = new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+}
+
+function dzienRoboczy() {
+    dzienRob = new Date();
+    d = dzienRob.getDay();
+    var today;
+    //Sunday=0, Monday=1 etc.
+    if (d == 1) { //jak poniedzialek to pokazac piatek
+        today = "1";
+        document.getElementById("dzienRoboczy").innerHTML = 6;
+    } else if (d == 2) {
+        today = "2";
+        document.getElementById("dzienRoboczy").innerHTML = today - 1;
+    } else if (d == 3) {
+        today = "3";
+        document.getElementById("dzienRoboczy").innerHTML = today - 1;
+    } else if (d == 4) {
+        today = "4";
+        document.getElementById("dzienRoboczy").innerHTML = today - 1;
+    } else if (d == 5) {
+        today = "5";
+        document.getElementById("dzienRoboczy").innerHTML = today - 1;
+    } else if (d == 6) {
+        today = "6";
+        document.getElementById("dzienRoboczy").innerHTML = today - 1;
+    }
+}
