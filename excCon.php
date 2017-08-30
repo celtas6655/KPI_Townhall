c49ea5db47030ff1c190a4ed10b8b480a5076c6a
Michal <hellpartyy@gmail.com>
2017-08-30 08:33:19 +0200

    before merge develop->master

--- excCon.php
+++ excCon.php
@@ -5,17 +5,20 @@ $sheet2 = "Arkusz1";
 $excel_app = new COM("Excel.application") or Die ("Did not connect");
 $Workbook = $excel_app->Workbooks->Open("$filename") or Die("Did not open $filename $Workbook");
 $Worksheet = $Workbook->Worksheets($sheet1);
+
 $Worksheet->activate;
 $excel_cell = $Worksheet->Range("A1");
 $excel_cell->activate;
 $excel_result = $excel_cell->value;
 print "$excel_result\n";
+
 $Worksheet = $Workbook->Worksheets($sheet2);
 $Worksheet->activate;
 $excel_cell = $Worksheet->Range("B1");
 $excel_cell->activate;
 $excel_result = $excel_cell->value;
 print "$excel_result\n";
+
 #To close all instances of excel:
 $Workbook->Close;
 unset($Worksheet);
