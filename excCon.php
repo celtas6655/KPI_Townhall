ed5310ac26ac373fd4d3afb52d1538b400130332
Michal <hellpartyy@gmail.com>
2017-08-30 14:28:13 +0200

    one after merge develop->master 502 bad gateway problem

--- excCon.php
+++ excCon.php
@@ -1,23 +1,26 @@
 <?php
-$filename = "C:\\Users\\tfd054it0154\\Desktop\\KPI_webApp\\data\\parsetest.xlsx";
-$sheet1 = "Arkusz1";
-$sheet2 = "Arkusz1";
+$filename = "C:\\Users\\tfd054it0154\\Desktop\\KPI_webApp\\data\\Output.xlsb";
+$sheet1 = "DAILY_OUTPUT";
+//$sheet2 = "Arkusz1";
 $excel_app = new COM("Excel.application") or Die ("Did not connect");
 $Workbook = $excel_app->Workbooks->Open("$filename") or Die("Did not open $filename $Workbook");
 $Worksheet = $Workbook->Worksheets($sheet1);
 
 $Worksheet->activate;
-$excel_cell = $Worksheet->Range("A1");
+$excel_cell = $Worksheet->Range("AN15");
 $excel_cell->activate;
 $excel_result = $excel_cell->value;
-print "$excel_result\n";
-
-$Worksheet = $Workbook->Worksheets($sheet2);
-$Worksheet->activate;
-$excel_cell = $Worksheet->Range("B1");
+//print "$excel_result\n";
+$excel_cell = $Worksheet->Range("AN16");
 $excel_cell->activate;
-$excel_result = $excel_cell->value;
-print "$excel_result\n";
+$excel_result2 = $excel_cell->value;
+//print "$excel_result2";
+//$Worksheet = $Workbook->Worksheets($sheet2);
+//$Worksheet->activate;
+//$excel_cell = $Worksheet->Range("B1");
+//$excel_cell->activate;
+//$excel_result = $excel_cell->value;
+//print "$excel_result\n";
 
 #To close all instances of excel:
 $Workbook->Close;
