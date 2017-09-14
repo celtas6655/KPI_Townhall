87d95089b1ca033e269e98e9c5041193c8a80ed5
Michal <hellpartyy@gmail.com>
2017-09-14 15:59:47 +0200

    refresh button, bigger font

--- bhp.php
+++ bhp.php
@@ -59,38 +59,37 @@
                 <!--                <div id="chart_div"></div>-->
                 <!--                <canvas id="cvs" width="300" height="300"></canvas>-->
                 <div id="chartContainer" style="height: 300px;width: 50%">
-                    <h6>BHP content</h6>
-                    <p id="demo"></p>
+                    <div class="counter"><h6>Dni bez wypadku(test): </h6>
+                    <h3 id="demo"></h3>
+<!--                        dodac buttona z resetem "countDownDate = new Date(); localStorage.setItem('startDate', countDownDate);" -->
                     <script type="text/javascript">
-                        // Set the date we're counting down to
-                        var countDownDate = new Date();
-                        document.write(countDownDate);
+                        var countDownDate = localStorage.getItem('startDate');
+                        if (countDownDate) {
+                            countDownDate = new Date(countDownDate);
+                        } else {
+                            countDownDate = new Date();
+                            localStorage.setItem('startDate', countDownDate);
+                        }
                         // Update the count down every 1 second
                         var x = setInterval(function() {
 
-                        // Get todays date and time
-                        var now = new Date().getTime();
+                            // Get todays date and time
+                            var now = new Date().getTime();
 
-                        // Find the distance between now an the count down date
-                        var distance = countDownDate.getTime() + now;
+                            // Find the distance between now an the count down date
+                            var distance = now - countDownDate.getTime();
 
-                        // Time calculations for days, hours, minutes and seconds
-                        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                            // Time calculations for days, hours, minutes and seconds
+                            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                        // Output the result in an element with id="demo"
-                        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-                        + minutes + "m " + seconds + "s ";
-
-                        // If the count down is over, write some text
-                        if (distance < 0) {
-                        clearInterval(x);
-                        document.getElementById("demo").innerHTML = "EXPIRED";
-                        }
+                            // Output the result in an element with id="demo"
+                            document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
                         }, 1000);
                     </script>
+                    </div>
                 </div>
             </div>
         </div>
